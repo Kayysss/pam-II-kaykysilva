a9a1b080ccf5654f4d13e842a32a9fea4c580069
Kayysss <kaysilva244@gmail.com>
2023-03-29 11:05:03 -0300

    Enviando arquivos da aula

---
+++ aula03/funcoesevariaveis.js
@@ -0,0 +1,18 @@
+let num1 = 2; 
+const num2 = 3
+var num3 = 5
+var msg = "Como vai?"
+let usuario = {
+    nome: "Kays", 
+    idade: 17,
+    signo: "Virgem",
+    calvo: false,
+    lindo: true
+}
+
+// console.log(`${msg} ${num3} aninhos. a primeira variável é do tipo ${typeof msg}. Já a segunda, é do tipo ${typeof num3}`)
+
+console.log(`${usuario.nome} é lindo e tem ${usuario.idade} anos, e ainda é ${usuario.signo} pois nasceu dia 23 de agosto!!!!`)
+
+console.log(typeof usuario.calvo)
+
